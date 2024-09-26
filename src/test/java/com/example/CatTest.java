84e46cdff85f3bb1f1ab6d0cbd4637f6946821ad
Zhumanali Temirlan <zhumanali2021@gmail.com>
2024-09-27 00:24:30 +0500

    wrote unit tests for the project

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,4 +1,36 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
-  
+    @Mock
+    private Feline feline;
+
+    @Test
+    public void testGetSound() {
+        Cat cat = new Cat(feline);
+        String result = cat.getSound();
+        System.out.println(result);
+        assertEquals("Мяу", result);
+    }
+
+    @Test
+    public void testGetFood()throws Exception{
+
+        Cat cat = new Cat(feline);
+
+        List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
+        assertEquals(expectedFood, cat.getFood());
+        System.out.println(cat.getFood());
+    }
 }
--- src/test/java/com/example/LionParameterizedTest.java
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -1,2 +1,42 @@
-package com.example;public class LionParameterizedTest {
+package com.example;
+
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class LionParameterizedTest {
+
+    private final String sex;
+    private final boolean expectedHasMane;
+
+    public LionParameterizedTest(String sex, boolean expectedHasMane) {
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        });
+    }
+
+    @Test
+    public void testDoesHaveMane() throws Exception {
+        Feline feline = new Feline();
+        Lion lion = new Lion(sex, feline);
+
+        boolean hasMane = lion.doesHaveMane();
+
+        Assert.assertEquals("Проверка наличия гривы: " + sex, expectedHasMane, hasMane);
+    }
 }
